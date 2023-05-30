522e3b37f2b150c29f4427e2b53fc8c3b503ba97
yuu <faisal.a2110@gmail.com>
2023-05-30 22:48:42 +0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider} from 'react-router-dom'
+import Home from './pages/home'
+import Login from './pages/login'
+import Signup from './pages/signup'
+import Product from './pages/product';
+
+const router = createBrowserRouter([
+  {
+    path: '/', element: <Home/>
+  },
+  {
+    path: '/login', element: <Login/>
+  },
+  {
+    path: '/signup', element: <Signup/>
+  },
+  {
+    path: '/product/basic-tee-tay-son', element: <Product id="1"/>
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <RouterProvider router={router}/>
 );
 
 // If you want to start measuring performance in your app, pass a function
